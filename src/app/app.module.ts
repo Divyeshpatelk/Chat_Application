24e536292eabe1dcaf6f09e2237e189a42bf71e3
Divyesh Patel <pateldivyesh343@gmail.com>
2018-04-20 13:19:44 +0530

    some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { HomeComponent } from './home/home.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { SingupComponent } from './singup/singup.component';
+
 @NgModule({
   declarations: [
     AppComponent,
